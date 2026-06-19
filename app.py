a0d5b0a3ec0062009f07604f4f65096a4185e811
yang6754 <3365823305@qq.com>
2026-06-20 01:12:24 +0800

    更新版本至 v2.0

--- app.py
+++ app.py
@@ -35,7 +35,7 @@ HTML = """<!DOCTYPE html>
 <body>
     <div class="card">
         <h1>🚀 CI/CD 部署成功！</h1>
-        <p class="version">Flask App v1.0 | Python {{ python_version }}</p>
+        <p class="version">Flask App v2.0 | Python {{ python_version }}</p>
         <div class="status">✅ 服务运行正常</div>
         <div class="info">
 	    <p><span>姓名: </span>黄晓洋</p>
